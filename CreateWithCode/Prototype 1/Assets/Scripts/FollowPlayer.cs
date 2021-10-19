806e8bd0429b67e6fe2877eeef6340836cf1cd4d
Guy French <guybfrench@gmail.com>
2021-10-19 09:58:33 -0600

    Finished the first part of creating with code practice

--- CreateWithCode/Prototype 1/Assets/Scripts/FollowPlayer.cs
+++ CreateWithCode/Prototype 1/Assets/Scripts/FollowPlayer.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
     public GameObject player;
-    public Vector3 offset = new Vector3(0, 5, -8);
+    private Vector3 offset = new Vector3(0, 5, -8);
 
     // Start is called before the first frame update
     void Start()
@@ -14,8 +14,9 @@ public class FollowPlayer : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
+        // Offset the camera behind the player by adding to the player's position
         transform.position = player.transform.position + offset;
     }
 }
