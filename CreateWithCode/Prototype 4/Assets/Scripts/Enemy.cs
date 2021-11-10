d60b4dfc8388e6d2c8ed659a2ee9497f9fab20a8
Guy French <guybfrench@gmail.com>
2021-11-09 23:15:33 -0700

    Finished Prototype 4

--- CreateWithCode/Prototype 4/Assets/Scripts/Enemy.cs
+++ CreateWithCode/Prototype 4/Assets/Scripts/Enemy.cs
@@ -7,6 +7,7 @@ public class Enemy : MonoBehaviour
     public float speed = 5.0f;
     private Rigidbody enemyRb;
     private GameObject player;
+    
 
 
     // Start is called before the first frame update
@@ -21,5 +22,9 @@ public class Enemy : MonoBehaviour
     {
         Vector3 lookDirection = (player.transform.position - transform.position).normalized;
         enemyRb.AddForce(lookDirection * speed);
+        if(transform.position.y < -10) 
+        { 
+            Destroy(gameObject); 
+        }
     }
 }
